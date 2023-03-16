0b42d21f3c65ed44ec15aa08750417c87de0d453
Muhammad Arsalan <mianarsalan20@gmail.com>
2023-03-16 15:49:17 +0500

    first commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -55,7 +55,7 @@ export const login = async (req, res) => {
     const token = jwt.sign(
       { id: user._id, role: user.role },
       process.env.JWT_SECRET_KEY,
-      { expiresIn: "1d" }
+      { expiresIn: "15d" }
     );
 
     //set token in cookie
